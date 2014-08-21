8ba5fb3ddcc6617c7813c86c0e126549ed3b6bac
emag <lanabe.lanabe@gmail.com>
2014-08-21 15:20:34 +0900

    Add controller method

---
+++ myrooapp/src/main/java/at/morec/springmvcroobook/myrooapp/FormData.java
@@ -0,0 +1,18 @@
+package at.morec.springmvcroobook.myrooapp;
+
+/**
+ * @author tanabe
+ */
+public class FormData {
+
+  private String input;
+
+  public String getInput() {
+    return input;
+  }
+
+  public void setInput(String input) {
+    this.input = input;
+  }
+
+}
--- myrooapp/src/main/java/at/morec/springmvcroobook/myrooapp/HelloController.java
+++ myrooapp/src/main/java/at/morec/springmvcroobook/myrooapp/HelloController.java
@@ -1,7 +1,10 @@
 package at.morec.springmvcroobook.myrooapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +21,14 @@ public class HelloController {
   }
 
   @RequestMapping
-  public String index() {
+  public String index(Model model) {
+    model.addAttribute("formData", new FormData());
+    return "hello/index";
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/form")
+  public String post(@ModelAttribute FormData form, Errors result, Model model) {
+    model.addAttribute("message", "You typed: " + form.getInput());
     return "hello/index";
   }
 
