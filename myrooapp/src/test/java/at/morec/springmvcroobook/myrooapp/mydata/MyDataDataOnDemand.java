28e3833c04b1ef77c53e6e2084a3dceb59def873
emag <lanabe.lanabe@gmail.com>
2014-08-21 15:49:51 +0900

    Very very great scaffoldgit st

---
+++ myrooapp/src/test/java/at/morec/springmvcroobook/myrooapp/mydata/MyDataDataOnDemand.java
@@ -0,0 +1,6 @@
+package at.morec.springmvcroobook.myrooapp.mydata;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = MyData.class)
+public class MyDataDataOnDemand {
+}
